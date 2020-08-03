475575669211857401088ed9e475088e72217e08
Thomas Dolan <tdolanindiana@yahoo.com>
2020-08-03 17:28:53 -0600

    Added simple login/logout

--- dataApp/views.py
+++ dataApp/views.py
@@ -1,29 +1,30 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Student, Course, Grade
 
 
 ############# HOME VIEW  ################
 
-class HomePageView(TemplateView):
+class HomePageView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
 
 
 ############# LIST VIEWS ################
 
 
-class StudentListView(ListView):
+class StudentListView(LoginRequiredMixin, ListView):
     model = Student
     template_name = 'student_list.html'
 
 
-class CourseListView(ListView):
+class CourseListView(LoginRequiredMixin, ListView):
     model = Course
     template_name = 'course_list.html'
 
 
-class GradeListView(ListView):
+class GradeListView(LoginRequiredMixin, ListView):
     model = Grade
     template_name = 'grade_list.html'
 
@@ -31,36 +32,36 @@ class GradeListView(ListView):
 ############# DETAIL VIEWS ################
 
 
-class StudentDetailView(DetailView):
+class StudentDetailView(LoginRequiredMixin, DetailView):
     model = Student
     template_name = 'student_detail.html'
 
 
-class CourseDetailView(DetailView):
+class CourseDetailView(LoginRequiredMixin, DetailView):
     model = Course
     template_name = 'course_detail.html'
 
 
-class GradeDetailView(DetailView):
+class GradeDetailView(LoginRequiredMixin, DetailView):
     model = Grade
     template_name = 'grade_detail.html'
 
 
 ############# CREATE VIEWS ################
 
-class StudentCreateView(CreateView):
+class StudentCreateView(LoginRequiredMixin, CreateView):
     model = Student
     template_name = 'student_create.html'
     fields = ('fname', 'lname', 'enrolled_since')
 
 
-class CourseCreateView(CreateView):
+class CourseCreateView(LoginRequiredMixin, CreateView):
     model = Course
     template_name = 'course_create.html'
     fields = ('dept', 'num', 'name', 'credits')
 
 
-class GradeCreateView(CreateView):
+class GradeCreateView(LoginRequiredMixin, CreateView):
     model = Grade
     template_name = 'course_create.html'
     fields = ('sid', 'course', 'letter_grade')
@@ -70,21 +71,21 @@ class GradeCreateView(CreateView):
 ############# UPDATE VIEWS ################
 
 
-class StudentUpdateView(UpdateView):
+class StudentUpdateView(LoginRequiredMixin, UpdateView):
     model = Student
     fields = ('fname', 'lname', 'enrolled_since')
     template_name = 'student_edit.html'
     success_url = reverse_lazy('student_list')
 
 
-class CourseUpdateView(UpdateView):
+class CourseUpdateView(LoginRequiredMixin, UpdateView):
     model = Course
     fields = ('dept', 'num', 'name', 'credits')
     template_name = 'course_edit.html'
     success_url = reverse_lazy('course_list')
 
 
-class GradeUpdateView(UpdateView):
+class GradeUpdateView(LoginRequiredMixin, UpdateView):
     model = Grade
     fields = ('sid', 'course', 'letter_grade')
     template_name = 'grade_edit.html'
@@ -94,19 +95,19 @@ class GradeUpdateView(UpdateView):
 ############# DELETE VIEWS ################
 
 
-class StudentDeleteView(DeleteView):
+class StudentDeleteView(LoginRequiredMixin, DeleteView):
     model = Student
     template_name = 'student_delete.html'
     success_url = reverse_lazy('student_list')
 
 
-class CourseDeleteView(DeleteView):
+class CourseDeleteView(LoginRequiredMixin, DeleteView):
     model = Course
     template_name = 'course_delete.html'
     success_url = reverse_lazy('course_list')
 
 
-class GradeDeleteView(DeleteView):
+class GradeDeleteView(LoginRequiredMixin, DeleteView):
     model = Grade
     template_name = 'grade_delete.html'
     success_url = reverse_lazy('grade_list')
--- eduData/settings.py
+++ eduData/settings.py
@@ -104,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
